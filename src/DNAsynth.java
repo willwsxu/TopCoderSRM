08534ce8b1f54ca5f058962460985e9b2f22ec02
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-02-11 11:48:45 -0600

    DNAsynth pass 5 tests, check for infinitoop

--- src/DNAsynth.java
+++ src/DNAsynth.java
@@ -17,42 +17,82 @@ import java.util.TreeSet;
  */
 public class DNAsynth {
     Map<String, Set<String>>    graph=new TreeMap<>();
+    Set<String> allSeq = new TreeSet<>(); // contain all SEQ fragments
+    
+    // seq1:seq2, any DNA start with seq2 can be appended to DNA ends with seq1
+    private void addToMap(String seq1, String seq2)
+    {
+        graph.compute(seq1, (k,v)->{
+            if (v==null)
+                v = new TreeSet<>();
+            v.add(seq2);
+            // any seq begin with seq2 can be connected to seq1
+            for (String seq : allSeq)
+                if (seq.startsWith(seq2))
+                    v.add(seq);
+            return v;
+        });        
+    }
     public int longest(String[] reactivity)
     {
+        for (String elem : reactivity) {            
+            String [] pair = elem.split("[\\:]");
+            for (String e: pair) {
+                allSeq.add(e);
+                allSeq.add(new StringBuilder(e).reverse().toString());
+            }
+        }
         for (String elem : reactivity) {
             String [] pair = elem.split("[\\:]");
-            graph.compute(pair[0], (k,v)->{
-                if (v==null) {
-                    Set<String> s = new TreeSet<>();
-                    s.add(pair[1]);
-                    return s;
-                } else
-                {
-                    v.add(pair[1]);
-                    return v;
-                }
-            });
-            
+            addToMap(pair[0], pair[1]);            
             // reverse reactivity mapping
-            graph.compute(new StringBuilder(pair[1]).reverse().toString(), (k,v)->{
-                String val = new StringBuilder(pair[0]).reverse().toString();
-                if (v==null) {
-                    Set<String> s = new TreeSet<>();
-                    s.add(val);
-                    return s;
-                } else
-                {
-                    v.add(val);
-                    return v;
-                }
-            });
+            addToMap(new StringBuilder(pair[1]).reverse().toString(), 
+                    new StringBuilder(pair[0]).reverse().toString());           
         }
         out.println(graph);
-        return -1;
+        
+        graph.forEach((k,v)->{
+        for (String seq2 : v)  // start with any initial pair
+            fullSearch(k+seq2);});
+        out.println(longDNA+":"+longDNA.length());
+
+        if (loop)
+            return -1;
+        return longDNA.length();
+    }
+    String longDNA="";
+    boolean loop=false;
+    void fullSearch(String DNA)
+    {
+        graph.forEach((k,v)->{if (DNA.endsWith(k))
+        {
+            for (String seq2: v) {
+                if (DNA.startsWith(seq2)) {
+                    longDNA="";
+                    loop = true;
+                    return;
+                }
+                if (loop) {  // skip 
+                    return;
+                }
+                fullSearch(DNA+seq2);
+            }
+        }else {
+            if (loop) {  // skip 
+                return;
+            }
+            if (DNA.length()>longDNA.length())
+                longDNA = DNA;
+        }});
     }
     
     public static void main(String[]args)
     {
+        new DNAsynth().longest(new String[]{"TTA:AGG"});
         new DNAsynth().longest(new String[]{"TTA:AGG","AGG:CCC"});
+        new DNAsynth().longest(new String[]{"CCC:AAA","AAA:CCC"});
+        new DNAsynth().longest(new String[]{"AG:AC","AT:GC","GC:AG"});
+        new DNAsynth().longest(new String[]{"TAG:ATC","GCA:CCCT","GAT:AC"});
+        new DNAsynth().longest(new String[]{"TG:GA","CGGG:TGG","GGA:AAAC"});
     }
 }

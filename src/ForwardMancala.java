2ca161a2da600bef7ab062f8f549054ce2b19722
ANDY-PROBOOK\Will <willwsxu@gmail.com>
2017-03-09 22:13:42 -0600

    ForwardMancala pass all tests

--- src/ForwardMancala.java
+++ src/ForwardMancala.java
@@ -24,9 +24,28 @@ public class ForwardMancala {
         }
         out.println();
     }
+    int findMax(int m[])
+    {
+        int max=0;
+        for(int i=0; i<m.length; i++) {
+            if (max<m[i])
+                max = m[i];
+        }
+        //out.println("max "+max);
+        return max;
+    }
+    int skipZero(int start[], int pos)
+    {        
+        for (int i=0; i<start.length; i++) {
+            if ( start[pos]>0 )
+                break;
+            pos = (pos+1)%start.length;
+        }
+        return pos;
+    }
     int move(int start[], int pos)
     {
-        out.println(pos);
+        //out.println("pos "+pos);
         int stones = start[pos];
         start[pos]=0;
         int next = (pos+1)% start.length;
@@ -34,7 +53,7 @@ public class ForwardMancala {
             pos = (pos+1)%start.length;
             start[pos]++;
         }
-        print(start);
+        //print(start);
         return next;        
     }
     // start length 2 to 10
@@ -51,38 +70,41 @@ public class ForwardMancala {
                 pos=i;
             }
         }
-        out.println(max);
-        out.println(total);
-        out.println(pos);
+        //out.println("max "+max);
+        //out.println("total "+total);
         if (max==total)
             return new int[]{};
         
         ArrayList<Integer> moves = new ArrayList<>(100);
-        // find first move
-        for (int i=0; i<start.length; i++) {
-            pos = (pos+1)%start.length;
-            if ( start[pos]>0 )
-                break;
-        }
+        // first move
+        ++pos;
         while (moves.size()<2500) {
+            pos = skipZero(start, pos);
             moves.add(pos);
             pos = move(start, pos);
             if (start[pos]==total)
                 break;
+            if (start[pos]==findMax(start) && start[pos] != 1) {
+                int next = (pos+1)%start.length;
+                if ( start[pos]>start[next])  // skip only if next item is not same
+                    pos = next;
+            }
         }
         int a[]=new int[moves.size()];
         for (int i=0; i<moves.size(); i++) {
             a[i] = moves.get(i);
         }
-        out.println(moves);
+        out.println(moves.size()+": "+moves);
         return a;
     }
     
     static void test()
     {
-        //new ForwardMancala().findMoves(new int[]{6,1,0,1});
-        //new ForwardMancala().findMoves(new int[]{0,10,0,0,0});
+        new ForwardMancala().findMoves(new int[]{6,1,0,1});
+        new ForwardMancala().findMoves(new int[]{0,10,0,0,0});
+        new ForwardMancala().findMoves(new int[]{0,1,0,1,0,1});
         new ForwardMancala().findMoves(new int[]{5,0,0,1,3,0,2,0});
+        new ForwardMancala().findMoves(new int[]{10,10,10,10,10,10,10,10,10,10});
     }
     
     public static void main(String[] args) {
